11cf529151d905eb2013a0522bc62aed2e60f19b
Syrillings <sinenernest43@gmail.com>
2025-07-30 17:49:43 +0100

    Inital Commit

---
+++ Ramblr.sql
@@ -0,0 +1,5 @@
+-- SQLite
+--So, basically, what's happening here is that you can use this
+-- sql query file to view the db of your choice...I'll explain better over
+-- a call
+select * from Users
