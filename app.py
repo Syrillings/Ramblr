054c4be1054a136a5e522bd428dc4786028c3211
Attah Sunday <attahsundayjr@gmail.com>
2025-07-31 15:57:05 +0100

    Created the about page and added the route/endpoint for the page to the app.py file

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def landing():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 
 def init_db():
     conn = sqlite3.connect("Ramblr.db")
@@ -69,6 +65,10 @@ def signup():
             conn.close()
             return render_template('signup.html', error="Email already exists.")
     return render_template('signup.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
